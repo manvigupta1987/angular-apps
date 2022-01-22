d6a0d4e131c04aaeb691f9f6819e7bd343f9d6af
Manvi <manvigupta1987@gmail.com>
2022-01-22 05:15:06 -0500

    recipe-app: http changes

--- recipe-app/src/app/recipes/recipe.service.ts
+++ recipe-app/src/app/recipes/recipe.service.ts
@@ -11,21 +11,23 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   // this also can be replaced with Subject 
 
-  private recipes:Recipe[] = [
-    new Recipe('Tasty Pasta', 'A super creamy Pasta', 'https://cdn.pixabay.com/photo/2016/07/13/16/19/creamy-pasta-1514893_1280.jpg', [
-      new Ingredient("Pasta", 1),
-      new Ingredient("Garlic", 1),
-      new Ingredient("Spinach", 1),
-      new Ingredient("Cream Cheese", 1),  
-    ]),
-    new Recipe('A Big fat Burger', 'What else do you want to say? ', 'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg', [
-      new Ingredient("Buns", 2),
-      new Ingredient("Chicken Patty", 1),
-      new Ingredient("Cheese", 1),
-      new Ingredient("Onions", 1),
-      new Ingredient("Tomato", 1),
-    ])
-  ];
+  // private recipes:Recipe[] = [
+  //   new Recipe('Tasty Pasta', 'A super creamy Pasta', 'https://cdn.pixabay.com/photo/2016/07/13/16/19/creamy-pasta-1514893_1280.jpg', [
+  //     new Ingredient("Pasta", 1),
+  //     new Ingredient("Garlic", 1),
+  //     new Ingredient("Spinach", 1),
+  //     new Ingredient("Cream Cheese", 1),  
+  //   ]),
+  //   new Recipe('A Big fat Burger', 'What else do you want to say? ', 'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg', [
+  //     new Ingredient("Buns", 2),
+  //     new Ingredient("Chicken Patty", 1),
+  //     new Ingredient("Cheese", 1),
+  //     new Ingredient("Onions", 1),
+  //     new Ingredient("Tomato", 1),
+  //   ])
+  // ];
+
+  private recipes:Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
@@ -39,6 +41,11 @@ export class RecipeService {
 
   getRecipe(id:number) {
     return this.recipes[id];
+  }
+
+  setRecipes(recipes: Recipe[] ) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   saveRecipe(recipe: Recipe) {
--- recipe-app/src/app/shared/data-storage.service.ts
+++ recipe-app/src/app/shared/data-storage.service.ts
@@ -2,27 +2,44 @@ import { Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private httpClient:HttpClient, private recipeService: RecipeService) {
-
-  }
+  constructor(
+    private httpClient: HttpClient,
+    private recipeService: RecipeService
+  ) {}
 
   saveData() {
     const recipes = this.recipeService.getRecipes();
-    this.httpClient.put('https://recipe-app-angular-335fa-default-rtdb.firebaseio.com/recipes.json',
-        recipes).subscribe((resData)=> {
-          console.log(resData);
-        });
+    this.httpClient
+      .put(
+        'https://recipe-app-angular-335fa-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe(resData => {
+        console.log(resData);
+      });
   }
 
   fetchData() {
-    this.httpClient.get('https://recipe-app-angular-335fa-default-rtdb.firebaseio.com/recipes.json')
-      .subscribe((resData)=>{
-    console.log(resData);       
-  })
+    this.httpClient
+      .get<Recipe[]>(
+        'https://recipe-app-angular-335fa-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe, 
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          }
+        })
+      }))
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
